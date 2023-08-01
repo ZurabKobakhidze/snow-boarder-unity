fe5b8f221af4a8204c27963dde57667d51d4afe3
ZurabKobakhidze <zukakobakhidze@gmail.com>
2023-08-01 15:39:39 +0400

    add delay restart

--- Assets/scripts/CrashDetector.cs
+++ Assets/scripts/CrashDetector.cs
@@ -5,13 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float LoadDelay = 0.5f;
     // Start is called before the first frame update
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Ground")
+        if (other.tag == "Ground")
         {
-           SceneManager.LoadScene(0);
+            Invoke("ReloadScene", LoadDelay);
         }
 
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/scripts/FinishLine.cs
+++ Assets/scripts/FinishLine.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
+
+    [SerializeField] float LoadDelay = 0.5f;
     // Start is called before the first frame update
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            SceneManager.LoadScene(0);
+            Invoke("ReloadScene", LoadDelay);
         }
 
+    }
+
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
